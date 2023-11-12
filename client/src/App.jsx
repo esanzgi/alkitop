dc1656d4645271b7c1bee51dc6c1dd6f06cb8fd9
esanzgi <esanzgi22wg@ikzubiri.com>
2023-11-12 16:25:59 +0100

    Header egina

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Header } from './components/Header'
+
+function App() {
+
+  return (
+    <main className='container-fluid'>
+      <Header />
+    </main>
+  )
+}
+
+export default App
