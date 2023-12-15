b35b7a7f22fd7462e3883d72241deef2657f5dd6
Ekkaitz <erubioel22wg@ikzubirimanteo.com>
2023-12-15 10:08:54 +0100

    Kontrol panela

---
+++ server/resources/js/Pages/PowerBiPage.jsx
@@ -0,0 +1,12 @@
+// App.js
+
+import React from 'react'
+import { Powerbi } from '@/components/Powerbi'
+
+function PowerBiPage() {
+  return (
+    <Powerbi/>
+  )
+}
+
+export default PowerBiPage 
