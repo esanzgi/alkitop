cdd0bae713a3d104379f7d8c6dd4cc303dbfa58a
Ekkaitz <erubioel22wg@ikzubirimanteo.com>
2024-01-17 13:00:59 +0100

    email verification

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Models\Owner;
 use App\Models\Product;
 use App\Models\Rating;
@@ -13,7 +13,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
